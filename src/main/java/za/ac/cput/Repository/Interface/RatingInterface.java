12beefbb07aa2e46aa07f5508cc0e4ca53f01420
katia101 <217149073@mycput.ac.za>
2022-08-12 21:15:03 +0200

    connected to DB and added JpaRepository

--- src/main/java/za/ac/cput/Repository/Interface/RatingInterface.java
+++ src/main/java/za/ac/cput/Repository/Interface/RatingInterface.java
@@ -1,11 +1,14 @@
 package za.ac.cput.Repository.Interface;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.Entity.Rating;
 import za.ac.cput.Repository.iml.iRepository;
 
 import java.util.Set;
 
-public interface RatingInterface extends iRepository<Rating, String> {
+@Repository
+public interface RatingInterface extends JpaRepository<Rating, String> {
 
-    public Set<Rating> getAll();
+//    public Set<Rating> getAll();
 }
