dd20d09c6e20fe1dc8d918613269b224b203e87c
Nikitha Mbokotwana <Nikitha@pandoran.co.za>
2022-10-19 00:28:46 +0200

    Amendments

--- src/main/java/za/ac/cput/Repository/Interface/iReceiptRepository.java
+++ src/main/java/za/ac/cput/Repository/Interface/iReceiptRepository.java
@@ -3,10 +3,9 @@ package za.ac.cput.Repository.Interface;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.Entity.Receipt;
-import za.ac.cput.Repository.iml.iRepository;
 
-import java.util.Set;
+
 @Repository
 public interface iReceiptRepository extends JpaRepository<Receipt,String> {
-//    public Set<Receipt> getAll();
+
 }
---
+++ src/main/java/za/ac/cput/Service/impl/ReceiptServicesImpl.java
@@ -0,0 +1,53 @@
+/**ReceiptServicesImpl.java
+ * Service for Receipt
+ * Author: Nikitha Mbokotwana (218337906)
+ * Date: 17 October 2022
+ **/
+package za.ac.cput.Service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.Entity.Receipt;
+import za.ac.cput.Service.Interface.IReceiptService;
+import za.ac.cput.Repository.Interface.iReceiptRepository;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class ReceiptServicesImpl implements IReceiptService {
+    @Autowired
+    private iReceiptRepository repository;
+
+    @Override
+    public Receipt create(Receipt receipt) {
+        return this.repository.save(receipt);
+    }
+
+    @Override
+    public Receipt read(String s) {
+        return this.repository.findById(s).orElseGet(null);
+    }
+
+    @Override
+    public Receipt update(Receipt receipt) {
+        if(this.repository.existsById(receipt.getReceiptID())){
+            return this.repository.save(receipt);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s) {
+        this.repository.deleteById(s);
+        if (this.repository.existsById(s))
+            return false;
+        else
+            return true;
+    }
+
+    @Override
+    public Set<Receipt> getAll() {
+        return this.repository.findAll().stream().collect(Collectors.toSet());
+    }
+}
