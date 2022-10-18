a36ae9f5f82be5ae3db7e1b53c389326015f7d5b
Nikitha Mbokotwana <Nikitha@pandoran.co.za>
2022-10-19 00:29:30 +0200

    Add Controller files

---
+++ src/main/java/za/ac/cput/Controller/PaymentController.java
@@ -0,0 +1,9 @@
+/**PaymentController.java
+ * Controller for Payment
+ * Author: Nikitha Mbokotwana (218337906)
+ * Date: 17 October 2022
+ **/
+package za.ac.cput.Controller;
+
+public class PaymentController {
+}
